6262fb531d0bf7572a02878fc2fb0aca8437e7b7
praveen <reddypraveen9876@gmail.com>
2024-12-31 22:30:07 +0530

    login, logs btn added

--- src/components/TradeForm.js
+++ src/components/TradeForm.js
@@ -9,12 +9,14 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  CircularProgress,
 } from "@mui/material";
 import {
   getInstruments,
   getNifty50Value,
   getBnkNiftyValue,
 } from "../services/api";
+import { LOGIN_URL, LOGS_URL } from "../utils/links/URLs";
 
 const TradeForm = ({
   onGenerateToken,
@@ -24,16 +26,17 @@ const TradeForm = ({
   onStopTrading,
 }) => {
   const [tokenData, setTokenData] = useState("");
-  // const [instrumentCE, setInstrumentCE] = useState("NIFTY 24800 CE 24 OCT 24");
-  // const [instrumentPE, setInstrumentPE] = useState("NIFTY 24800 PE 24 OCT 24");
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
   const [snackbarSeverity, setSnackbarSeverity] = useState("success");
 
   const [savedInstruments, setSavedInstruments] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [apiLoading, setApiLoading] = useState(false); // New state to track API loading
 
   useEffect(() => {
     const fetchInstruments = async () => {
+      setApiLoading(true); // Set loading state to true while fetching data
       try {
         const response = await getInstruments();
         const instrumentsData = response.data.map((instrument) => ({
@@ -42,13 +45,14 @@ const TradeForm = ({
           putOptionSymbol: instrument.putOptionSymbol,
           updatedAt: instrument.updatedAt,
         }));
-        console.log(instrumentsData);
         setSavedInstruments(instrumentsData);
       } catch (error) {
         console.error("Error fetching instruments", error);
+      } finally {
+        setApiLoading(false); // Set loading state to false once the data is fetched
       }
     };
-
+    setApiLoading(true);
     fetchInstruments();
   }, []);
 
@@ -57,6 +61,7 @@ const TradeForm = ({
   };
 
   const handleGenerateToken = async () => {
+    setLoading(true);
     try {
       await onGenerateToken(tokenData);
       setSnackbarMessage("Token generated successfully!");
@@ -66,36 +71,13 @@ const TradeForm = ({
       setSnackbarMessage("Failed to generate token.");
       setSnackbarSeverity("error");
       setSnackbarOpen(true);
+    } finally {
+      setLoading(false);
     }
   };
 
-  // const handleSubmitCE = async () => {
-  //   try {
-  //     await onSubmitInstrumentKeyCE(instrumentCE);
-  //     setSnackbarMessage("Instrument Key CE submitted successfully!");
-  //     setSnackbarSeverity("success");
-  //     setSnackbarOpen(true);
-  //   } catch (error) {
-  //     setSnackbarMessage("Failed to submit Instrument Key CE.");
-  //     setSnackbarSeverity("error");
-  //     setSnackbarOpen(true);
-  //   }
-  // };
-
-  // const handleSubmitPE = async () => {
-  //   try {
-  //     await onSubmitInstrumentKeyPE(instrumentPE);
-  //     setSnackbarMessage("Instrument Key PE submitted successfully!");
-  //     setSnackbarSeverity("success");
-  //     setSnackbarOpen(true);
-  //   } catch (error) {
-  //     setSnackbarMessage("Failed to submit Instrument Key PE.");
-  //     setSnackbarSeverity("error");
-  //     setSnackbarOpen(true);
-  //   }
-  // };
-
   const handleStartTrading = async () => {
+    setLoading(true);
     try {
       await onStartTrading();
       setSnackbarMessage("Trading started successfully!");
@@ -105,10 +87,13 @@ const TradeForm = ({
       setSnackbarMessage("Failed to start trading.");
       setSnackbarSeverity("error");
       setSnackbarOpen(true);
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleStopTrading = async () => {
+    setLoading(true);
     try {
       await onStopTrading();
       setSnackbarMessage("Connection Closed Successfully!");
@@ -118,78 +103,80 @@ const TradeForm = ({
       setSnackbarMessage("Failed to stop trading.");
       setSnackbarSeverity("error");
       setSnackbarOpen(true);
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleNifty50Data = async () => {
+    setApiLoading(true); // Set loading state to true while fetching data
     try {
       const data = await getNifty50Value();
-      console.log(data, "cc");
       setSnackbarMessage(data.data.message);
-      // setSnackbarSeverity("success");
       setSnackbarOpen(true);
     } catch (error) {
       setSnackbarMessage("Failed setting Nifty 50 Data");
       setSnackbarSeverity("error");
+    } finally {
+      setApiLoading(false); // Set loading state to false once the data is fetched
     }
   };
 
   const handleBankNiftyData = async () => {
+    setApiLoading(true); // Set loading state to true while fetching data
     try {
       const data = await getBnkNiftyValue();
       setSnackbarMessage(data.data.message);
-      // setSnackbarSeverity("success");An error occurred
       setSnackbarOpen(true);
     } catch (error) {
       setSnackbarMessage("Failed setting Nifty 50 Data");
       setSnackbarSeverity("error");
+    } finally {
+      setApiLoading(false); // Set loading state to false once the data is fetched
     }
   };
 
   return (
     <div className="trade-form-container">
+    <Grid container spacing={2} className="trade-form" alignItems="center">
+  {/* Token Input */}
+  <Grid item xs={6}>
+    <TextField
+      fullWidth
+      label="Token Data"
+      value={tokenData}
+      onChange={(e) => setTokenData(e.target.value)}
+    />
+    <Button variant="contained" onClick={handleGenerateToken} fullWidth>
+      Generate Token
+    </Button>
+  </Grid>
+
+  {/* Login and Logs Buttons */}
+  <Grid item xs={6}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+      <Button
+        variant="contained"
+        href={LOGIN_URL}
+        target="_blank"
+        fullWidth
+      >
+        Login
+      </Button>
+      <Button
+        variant="contained"
+        href={LOGS_URL}
+        target="_blank"
+        fullWidth
+        style={{ backgroundColor: "#607d8b", color: "white" }}
+      >
+        Logs
+      </Button>
+    </div>
+  </Grid>
+</Grid>
+
       <Grid container spacing={2} className="trade-form">
-        {/* Token Input */}
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Token Data"
-            value={tokenData}
-            onChange={(e) => setTokenData(e.target.value)}
-          />
-          <Button variant="contained" onClick={handleGenerateToken}>
-            Generate Token
-          </Button>
-        </Grid>
-
-        {/* Instrument CE Input */}
-        {/* <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Instrument Key CE"
-            placeholder="E.g. NIFTY 25400 CE 17 OCT 24"
-            value={instrumentCE}
-            onChange={(e) => setInstrumentCE(e.target.value)}
-          />
-          <Button variant="contained" onClick={handleSubmitCE}>
-            Submit CE
-          </Button>
-        </Grid> */}
-
-        {/* Instrument PE Input */}
-        {/* <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Instrument Key PE"
-            placeholder="E.g. NIFTY 25400 PE 17 OCT 24"
-            value={instrumentPE}
-            onChange={(e) => setInstrumentPE(e.target.value)}
-          />
-          <Button variant="contained" onClick={handleSubmitPE}>
-            Submit PE
-          </Button>
-        </Grid> */}
-
         {/* Start/Stop Trading Buttons */}
         <Grid item xs={3}>
           <Button variant="contained" onClick={handleStartTrading}>
@@ -216,6 +203,13 @@ const TradeForm = ({
           </Button>
         </Grid>
       </Grid>
+
+      {/* Loader */}
+      {(loading || apiLoading) && (
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+          <CircularProgress />
+        </div>
+      )}
 
       {/* Saved Instruments Table */}
       <div className="saved-instruments">
