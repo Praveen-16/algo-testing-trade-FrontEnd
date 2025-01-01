73ef5714c9cdf26dfd07f513e11736ec15943562
praveen <reddypraveen9876@gmail.com>
2025-01-01 16:24:36 +0530

    update LOGIN_URL to use production redirect URI

--- src/utils/links/URLs.js
+++ src/utils/links/URLs.js
@@ -1,2 +1,2 @@
-export const LOGIN_URL = "https://api.upstox.com/v2/login/authorization/dialog?response_type=code&client_id=9a1c6050-d377-4659-bce7-66f4aec2b0a8&redirect_uri=https://192.168.0.166:432/&state=";
+export const LOGIN_URL = "https://api.upstox.com/v2/login/authorization/dialog?response_type=code&client_id=9a1c6050-d377-4659-bce7-66f4aec2b0a8&redirect_uri=https://algo-testing-2.onrender.com/&state=";
 export const LOGS_URL = "https://dashboard.render.com/web/srv-csmp0jo8fa8c73a9r8v0/logs";
